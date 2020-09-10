4a3f8ed11929d78ca119be3ed470d8e529806a37
Dave Chao <dave.chao@silkrode.com.tw>
2020-09-10 14:12:52 +0800

    added MaterialTheme ui

--- app/src/main/java/com/dabenxiang/mvvm/view/splash/SplashFragment.kt
+++ app/src/main/java/com/dabenxiang/mvvm/view/splash/SplashFragment.kt
@@ -3,6 +3,7 @@ package com.dabenxiang.mvvm.view.splash
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -17,7 +18,9 @@ class SplashFragment : Fragment() {
         savedInstanceState: Bundle?
     ) = ComposeView(requireContext()).apply {
         setContent {
-            buildNewsStory()
+            MaterialTheme {
+                buildNewsStory()
+            }
         }
     }
 }
