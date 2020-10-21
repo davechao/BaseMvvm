ff52c9e496c99699846987c8add284aeb44c77b2
Dave Chao <dave.chao@silkrode.com.tw>
2020-10-21 11:45:31 +0800

    added graphql

--- app/src/main/java/com/dabenxiang/mvvm/view/splash/SplashFragment.kt
+++ app/src/main/java/com/dabenxiang/mvvm/view/splash/SplashFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import com.dabenxiang.mvvm.R
+import com.dabenxiang.mvvm.model.api.ApiResult.*
 import com.dabenxiang.mvvm.view.base.BaseFragment
+import timber.log.Timber
 
 class SplashFragment : BaseFragment() {
 
@@ -12,6 +14,17 @@ class SplashFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel.postResult.observe(viewLifecycleOwner, {
+            when (it) {
+                is Success -> Timber.d(it.result?.post?.title)
+                is Error -> onApiError(it.throwable)
+                else -> {
+                }
+            }
+        })
+
+        viewModel.getPostById("1")
     }
 
     override fun getLayoutId(): Int {
